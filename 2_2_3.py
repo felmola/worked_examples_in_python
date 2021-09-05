25bef219ba08381f8c26b38e9933ed3f631af93f
felmola <felmola@gmail.com>
2021-09-05 10:18:00 -0500

    Stylized 2.2.3

--- 2_2_3.py
+++ 2_2_3.py
@@ -7,33 +7,27 @@ from itertools import combinations
 
 # Constants:
 
+
 def main():
     # Create sample space:
-    urn = [1, 2, 3 ,4 ,5, 6]
+    urn = [1, 2, 3, 4, 5, 6]
     sample_space = list(combinations(urn, 3))
-    print(sample_space)
-    print(len(sample_space))
+    # Convert list_of_combinations_object into list_of_lists:
     sample_space = [list(i) for i in sample_space]
-    print(sample_space)
-    print(len(sample_space))
+    print("The sample space is: ", sample_space,)
+    print("The number of outcomes is: ", len(sample_space))
 
-    # Checks if the second smaller number from each sample outcome is a 3.
-    count = 0
+    # Checks if the second smaller number from each sample outcome is a 3:
     event = []
+    # If the second number in a list is a 3, then appends that list to event:
     for i in sample_space:
-        if find_second_smaller(i) == 3:
+        # Sorts each list from lower to higher so in each list the second number is the second smaller:
+        i.sort
+        # If the second element of the list is a three, append it to event:
+        if i[1] == 3:
             event.append(i)
-    print(event)
-
-
-def find_second_smaller(list):
-    """
-    :param list: takes every possible sample outcome from the sample space.
-    :return: the second smaller number.
-    """
-    list.sort
-    # print(list[1])
-    return list[1]
+    print("The sample space in event A is: ", event)
+    print("The number of outcomes in event A is: ", len(event))
 
 
 if __name__ == "__main__":
